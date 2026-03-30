64555c6f773df24ea0eed89b7c2310faa7da5381
tcocorullo <tomasc@udesa.edu.ar>
2026-03-30 17:46:34 -0300

    Termine de crear las clases del header y footer

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,16 +1,19 @@
 import React from "react";
 import {Link} from "react-router-dom"
+import "./Menu.css"
 
 function Menu (){
     return(
-        <ul className="nav">
-            <li className="nav-item"> <Link to="/" >Home</Link> </li>
-            <li className="nav-item"> <Link to="/Peliculas" >Películas</Link> </li>
-            <li className="nav-item"> <Link to="/Series" >Series</Link> </li>
-            <li className="nav-item"> <Link to="/Favoritos" >Favoritos</Link> </li>
-            <li className="nav-item log"> <Link to="/Login" >Login</Link> </li>
-            <li className="nav-item reg"> <Link to="/Register" >Register</Link> </li>
-        </ul>
+    <nav>
+        <ul className="nav nav-tabs my-4">
+                <li className="nav-item"> <Link className="nav-link" to="/" >Home</Link> </li>
+                <li className="nav-item"> <Link className="nav-link" to="/Peliculas" >Películas</Link> </li>
+                <li className="nav-item"> <Link className="nav-link" to="/Series" >Series</Link> </li>
+                <li className="nav-item"> <Link className="nav-link" to="/Favoritos" >Favoritos</Link> </li>
+                <li className="nav-item ml-auto"> <Link className="nav-link" to="/Register" >Register</Link> </li>
+                <li className="nav-item"> <Link className="nav-link" to="/Login" >Login</Link> </li>
+            </ul>
+    </nav>
     )
 }
 
