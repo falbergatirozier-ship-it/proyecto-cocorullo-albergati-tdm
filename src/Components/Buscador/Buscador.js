aa7aac1448dbe83ec691ebbed89b6142081bb9ea
falbergatirozier-ship-it <franciscoalbergati@Franciscos-MacBook-Air.local>
2026-03-30 16:44:22 -0300

    merge

---
+++ src/Components/Buscador/Buscador.js
@@ -0,0 +1,31 @@
+class Formulario extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { valor: '' };
+  }
+
+  evitarSubmit(event) {
+    event.preventDefault();
+  }
+
+  controlarCambios(event) {
+    this.setState(
+      { valor: event.target.value },
+      () => console.log(this.state.valor)
+    );
+  }
+
+  render() {
+    return (
+      <form onSubmit={(event) => this.evitarSubmit(event)}>
+        <label>Name:</label>
+        <input
+          type="text"
+          onChange={(event) => this.controlarCambios(event)}
+          value={this.state.valor}
+        />
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
