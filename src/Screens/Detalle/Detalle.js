663d475a9cb5d30944d7bd578cf3061f5f676603
tcocorullo <tomasc@udesa.edu.ar>
2026-04-13 14:55:56 -0300

    Arregle el link a detalle

---
+++ src/Screens/Detalle/Detalle.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+
+class Detalle extends Component {
+    constructor () {
+        super ();
+
+    }
+    
+    render () {
+        return(
+            <div>Detalle</div>
+        )
+    }
+}
+
+export default Detalle
