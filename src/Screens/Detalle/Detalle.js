18c52e4f7c20157bfe51eabe84eb3263ddc75fa8
tcocorullo <tomasc@udesa.edu.ar>
2026-04-15 21:54:04 -0300

    Hice el boton de favoritos

--- src/Screens/Detalle/Detalle.js
+++ src/Screens/Detalle/Detalle.js
@@ -5,7 +5,8 @@ class Detalle extends Component {
         super (props);
         this.state = {
             datos: null,
-            tipo: this.props.match.params.tipo
+            tipo: this.props.match.params.tipo,
+            esFav: false
         }
 
     }
@@ -17,14 +18,57 @@ class Detalle extends Component {
         fetch ("https://api.themoviedb.org/3/" + tipo + "/" + id + "?api_key=" + apiKey)
             .then (res => res.json())
             .then (data => {
-                console.log('DATA', data)
-                this.setState(
-                {
-                    datos: data,
-                }, ()=> console.log(this.state)
-            )})
-        .catch (e => console.log(e))
-        console.log(this.state.datos);   
+                let storage = localStorage.getItem("favoritos")
+                let storageParseado = JSON.parse(storage)
+
+                if (storageParseado !== null) {
+                    let estaEnFav = storageParseado.includes(data.id)
+
+                    this.setState({
+                        datos: data,
+                        esFav: estaEnFav
+                    })
+                } else {
+                    this.setState({
+                        datos: data,
+                        esFav: false
+                    })
+                }
+            })
+        .catch (e => console.log(e))   
+        console.log(this.state.datos)
+    }
+
+    agregarFav (id) {
+        let storage = localStorage.getItem("favoritos")
+        let storageParseado = JSON.parse(storage)
+
+        if (storageParseado === null) {
+            let primerFav = [id]
+            let storageString = JSON.stringify(primerFav)
+            localStorage.setItem("favoritos",storageString)
+        } else {
+            storageParseado.push(id)
+            let storageString = JSON.stringify(storageParseado)
+            localStorage.setItem("favoritos",storageString)
+        }
+
+        this.setState({
+            esFav: true
+        })
+    }
+
+    sacarFav (id){
+        let storage = localStorage.getItem("favoritos")
+        let storageParseado = JSON.parse(storage)
+
+        let storageFiltrado = storageParseado.filter(idx => idx !== id)
+        let storageString = JSON.stringify(storageFiltrado)
+        localStorage.setItem("favoritos",storageString)
+
+        this.setState({
+            esFav: false
+        })
     }
     
     render () {
@@ -46,6 +90,12 @@ class Detalle extends Component {
                                     <p className="mt-0 mb-0" id="release-date"><strong>Fecha de estreno:</strong> {this.state.datos.release_date}</p>
                                     <p className="mt-0 mb-0 length"><strong>Duración:</strong> {this.state.datos.runtime}</p>
                                     <p className="mt-0" id="votes"><strong>Puntuación:</strong> {this.state.datos.vote_average}</p>
+                                    {
+                                        this.state.esFav == false ?
+                                        <button onClick={() => this.agregarFav(this.state.datos.id)}>🩶</button>
+                                        :
+                                        <button onClick={() => this.sacarFav(this.state.datos.id)}>♥️</button>
+                                    }
                                 </section>
                             </section>
                         </div>
@@ -63,6 +113,12 @@ class Detalle extends Component {
                                     <p className="mt-0 mb-0" id="release-date"><strong>Fecha de estreno:</strong> {this.state.datos.first_air_date}</p>
                                     <p className="mt-0 mb-0 length"><strong>Número de temporadas:</strong> {this.state.datos.number_of_seasons}</p>
                                     <p className="mt-0" id="votes"><strong>Puntuación:</strong> {this.state.datos.vote_average}</p>
+                                    {
+                                        this.state.esFav == false ?
+                                        <button onClick={() => this.agregarFav(this.state.datos.id)}>🩶</button>
+                                        :
+                                        <button onClick={() => this.sacarFav(this.state.datos.id)}>♥️</button>
+                                    }
                                 </section>
                             </section>
                         </div>
