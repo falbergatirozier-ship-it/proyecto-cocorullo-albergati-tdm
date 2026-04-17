dfc260c1fac8a1f6f3264c7b9aa15a3e68f5f9fd
tcocorullo <tomasc@udesa.edu.ar>
2026-04-16 21:17:53 -0300

    Agregue el boton de fav a series card

--- src/Components/CardSerie/CardSerie.js
+++ src/Components/CardSerie/CardSerie.js
@@ -6,10 +6,58 @@ class CardSerie extends Component {
     constructor (props) {
         super (props);
         this.state = {
-            verMas: false
+            verMas: false,
+            esFav: false
         }
         }
-    
+
+    componentDidMount () {
+        let storage = localStorage.getItem("favoritostv")
+        let storageParseado = JSON.parse(storage)
+
+        if (storageParseado !== null) {
+            let estaEnFav = storageParseado.includes(this.props.data.id)
+
+            this.setState({
+                esFav: estaEnFav
+            })
+        }
+}
+
+
+    agregarFav (id) {
+        let tipo = "tv"
+        let storage = localStorage.getItem("favoritos" + tipo)
+        let storageParseado = JSON.parse(storage)
+
+        if (storageParseado === null) {
+            let primerFav = [id]
+            let storageString = JSON.stringify(primerFav)
+            localStorage.setItem("favoritos" + tipo,storageString)
+        } else {
+            storageParseado.push(id)
+            let storageString = JSON.stringify(storageParseado)
+            localStorage.setItem("favoritos" + tipo,storageString)
+        }
+
+        this.setState({
+            esFav: true
+        })
+    }
+
+    sacarFav (id){
+        let tipo = "tv"
+        let storage = localStorage.getItem("favoritos" + tipo)
+        let storageParseado = JSON.parse(storage)
+
+        let storageFiltrado = storageParseado.filter(idx => idx !== id)
+        let storageString = JSON.stringify(storageFiltrado)
+        localStorage.setItem("favoritos" + tipo,storageString)
+
+        this.setState({
+            esFav: false
+        })
+    }
     
     verMas(){
         this.state.verMas ? this.setState({verMas: false}): this.setState({verMas: true})
@@ -25,6 +73,12 @@ class CardSerie extends Component {
                     <p className={this.state.verMas ? "card-text show" : "card-text hide"}>{this.props.data.overview}</p>
                     <button className="mar btn btn-primary" onClick={() => this.verMas()}>{this.state.verMas ? "Ver Menos" : "Ver Más"}</button>
                     <Link className="mar btn btn-primary" to={"/Detalle/tv/" + this.props.data.id}>Detalle</Link>
+                    {
+                        this.state.esFav == false ?
+                        <button onClick={() => this.agregarFav(this.props.data.id)}>🩶</button>
+                        :
+                        <button onClick={() => this.sacarFav(this.props.data.id)}>♥️</button>
+                    }
                 </div>
             </article>
         )
